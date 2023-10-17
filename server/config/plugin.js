eb2ca5f7408f29cf90ca64c625acb2a411d1640f
Dev Code <hellodev_code@outlook.com>
2023-10-17 21:08:17 +0530

    added vercel prod automation

---
+++ server/config/plugin.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_yBVTAeb9bfkCc8RurDKOywQQr56u/Ai1C7lZ7aG",
+      apiToken: "M2LS1eXY5cPQD1yrbNGylMVi",
+    },
+  },
+});
